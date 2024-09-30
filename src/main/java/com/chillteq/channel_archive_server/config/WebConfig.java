cc9c9e6043a944ec126ab162d03316f26db67167
= <=>
2024-09-30 19:36:27 -0400

    Update WebConfig to enable PUT/POST requests too

--- src/main/java/com/chillteq/channel_archive_server/config/WebConfig.java
+++ src/main/java/com/chillteq/channel_archive_server/config/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
     }
