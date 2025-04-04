40d05163976e18ae40c7f28fffb2c445fa6491c5
chriszilla1123 <chriszilla1123@gmail.com>
2025-04-04 00:32:39 -0400

    20250404 : exception handling for DownloadController class

---
+++ src/main/java/com/chillteq/channel_archive_server/model/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.chillteq.channel_archive_server.model;
+
+public class ErrorResponse {
+    private int statusCode;
+    private String message;
+
+    public ErrorResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
