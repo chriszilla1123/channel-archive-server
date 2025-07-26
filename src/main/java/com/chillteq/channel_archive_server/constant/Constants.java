70f9f838a7f79521efae9e0c632b9fb2e439e113
chriszilla1123 <chriszilla1123@gmail.com>
2025-07-26 19:56:16 -0400

    20250726 : Add cookies file as an optional parameter

--- src/main/java/com/chillteq/channel_archive_server/constant/Constants.java
+++ src/main/java/com/chillteq/channel_archive_server/constant/Constants.java
@@ -1,8 +1,10 @@
 package com.chillteq.channel_archive_server.constant;
 
 public class Constants {
+    public static final String ytdlPath = "/root/.local/bin/yt-dlp";
     public static final String userDefinedConfigFileLocation = "/userDefined.config";
     public static final String userDefinedHistoryFileLocation = "/history.json";
     public static final String exampleConfigResourceName = "example.config";
     public static final String baseVideoDirectory = "/baseDirectory";
+    public static final String cookiesFileLocation = "/cookies.txt";
 }
